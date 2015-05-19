18e2581fa57f4519735460d7e61dec6fc211ee11
Katya Apsadykova <apsadykova@yandex.ru>
2015-05-20 00:09:15 +0300

    Aerostat

---
+++ aerostat1/js/countdown.js
@@ -0,0 +1,3 @@
+$(function () {
+      $('#defaultCountdown').countdown({until: new Date(2015, 9 - 1, 1)});
+    })
---
+++ aerostat1/js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.slider4').bxSlider({
+    slideWidth: 300,
+    minSlides: 2,
+    maxSlides: 4,
+    moveSlides: 1,
+    slideMargin: 10
+  });
+});
